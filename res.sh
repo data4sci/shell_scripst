351880dec22b724081e4d142d71d8c5d6095a324
data4sci <data4sci@gmail.com>
2020-08-31 18:34:03 +0200

    Update res.sh

--- res.sh
+++ res.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+################################################################################
+#                                                                              #
+# resize all *.jpg files in working directory                                  #
+#                                                                              #
+################################################################################
+
 for f in `find . -name "*.jpg"`
 do
     convert $f -resize 33% -quality 87 $f.SMALL.JPG
 done
-
-
-
